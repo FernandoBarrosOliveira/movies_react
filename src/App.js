5f779d3fee063a9cb8c7c2d786f0eea619ef937e
Fernando Barros <fernandobarros@fcmsistemas.com>
2017-11-08 12:44:05 -0300

    Commit Criacao das paginas home e top20

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import './css/fjp_style.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <header>    
+            <button className="menu-abrir"></button>
+            <nav className="navegacao col-gra-00"> 
+                <button className="menu-fechar"></button>
+                <ul className = "menu-principal">
+                    <li className="col-gra-00"><a className="col-gra-00" href="/">Home</a></li>
+                    <li className="col-gra-00"><a className="col-gra-00" href="#">Top 20</a></li>
+                    <li className="col-gra-00"><a className="col-gra-00" href="#">Menu 3</a></li>
+                    <li className="col-gra-00"><a className="col-gra-00" href="#">Menu 4</a></li>
+                </ul>
+            </nav>
+            <script src="menu.js"></script>            
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
     );
   }
 }
---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './css/fjp_style.css';
+
+
+class Top20 extends Component{
+    render(){
+        return (
+            
+        );
+    }
+}
+
+export default Top20;
