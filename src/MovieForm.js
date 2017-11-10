84be0674c02690b95b2b008944d218dd2f69afa3
Fernando Barros <fernandobarros@fcmsistemas.com>
2017-11-10 18:28:29 -0300

    Commit aplicando estilo no formulario de cadastro

--- src/MovieForm.js
+++ src/MovieForm.js
@@ -10,7 +10,7 @@ class MovieForm extends Component{
     render() {
 
         return(
-            <div >
+            <div  >
                 <h4 className="fnt-roboto-i-c">Cadastro de filmes</h4> 
                 <form  onSubmit={this._handleSubmit.bind(this)}>
                     
@@ -18,7 +18,7 @@ class MovieForm extends Component{
                         <input placeholder="Url imagem" ref={input => this._urlImage = input}/><br/>                        
                         <input placeholder="Titulo" ref={input => this._titulo = input}/><br/>                        
                         <textarea placeholder="Sinopse" className="materialize-textarea" ref={textarea => this._sinopse = textarea}></textarea>
-                        <button  type="submit">Enviar</button>
+                        <button className="cadastro"  type="submit">Enviar</button>
                     </div>
                 </form>
             </div>
